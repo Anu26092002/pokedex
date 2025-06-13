491b63eaeabb47f368c544fbb3e1c9d3149cf25d
Anurag Ghosh <78692178+Anu26092002@users.noreply.github.com>
2025-06-14 00:53:01 +0530

    FEATURE: Update paldea.js :tada:

--- scripts/paldea.js
+++ scripts/paldea.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke_container');
-const pokemons_number = 1008;
+const pokemons_number = 1025;
 const colors = {
 	Normal: '#d1d0a4',
 	Fire: '#d66214',
